252619ae3f1e4dd859bb43901e8c6c1def4672eb
rdvelazquez <ryan.velazquez0001@temple.edu>
2018-09-17 12:49:14 -0400

    working on fixing fel

--- app/models/msa.js
+++ app/models/msa.js
@@ -124,7 +124,46 @@ Msa.virtual("hyphy_friendly").get(function() {
   return hyphy_obj;
 });
 
+// To be renamed to "removeTreeFromNexusOrFasta"... currently getting "issue removing tree from file" error on fel... see line 95 of app/routes/fel.js.
 Msa.statics.removeTreeFromNexus = function(input_file_path, output_file_path){
+  return new Promise(function(resolve, reject) {
+    fs.readFile(file_path, function(err, data) {
+      if (err) reject(err);
+      var file_lines = data.toString().split("\n");
+      if (file_lines[0] != "#NEXUS") {
+        // If the file is not a nexus we will assume it is fasta (phylib support to come)
+        // Remove tree lines, lines that start with "(".
+        for (var i = 0; i < file_lines.length; i++) {
+          if (file_lines[i].startsWith("(")) {
+            delete file_lines[i];
+          }
+        }
+        var FastaStringWithoutTree = file_lines.join("\n");
+        fs.writeFile(output_file_path, FastaStringWithoutTree, function(err){
+          if(err) reject(err);
+          resolve();
+        });
+      } else {
+        var begin_tree_index = file_lines.indexOf("BEGIN TREES;");
+        if (begin_tree_index > -1) {
+          var end_tree_index = file_lines.indexOf("END;", begin_tree_index),
+            number_to_remove = end_tree_index - begin_tree_index + 1;
+          if (/\s/.exec(file_lines[end_tree_index + 1])) {
+            number_to_remove++;
+          }
+          file_lines.splice(begin_tree_index, number_to_remove);
+        }
+        var NexusStringWithoutTree = file_lines.join("\n");
+        fs.writeFile(output_file_path, NexusStringWithoutTree, function(err){
+          if(err) reject(err);
+          resolve();
+        });
+      }
+    });
+  });
+};
+
+Msa.statics.removeTreeFromNexus_Old = function(input_file_path, output_file_path){
   return new Promise(function(resolve, reject) {
     fs.readFile(input_file_path, function(err, data) {
       if (err) reject(err);
